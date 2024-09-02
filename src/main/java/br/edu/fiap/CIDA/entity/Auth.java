b4abc7e4dcde1774bf121d1213065f725eb5c9cc
jaciteixeira <jaci9306@gmail.com>
2024-09-02 01:07:14 -0300

    retirado imports não usaddos

--- src/main/java/br/edu/fiap/CIDA/entity/Auth.java
+++ src/main/java/br/edu/fiap/CIDA/entity/Auth.java
@@ -1,10 +1,7 @@
 package br.edu.fiap.CIDA.entity;
 
 import jakarta.persistence.*;
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotEmpty;
-import jakarta.validation.constraints.NotNull;
-import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.*;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
